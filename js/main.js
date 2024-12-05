db8c96348fb6f44379f93df34db6425d1c9c3af1
Thanhchinn <thanhchinn0523@gmail,com>
2024-12-05 19:04:26 +0700

    funtion_addcart

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+// Them SP vao gio hang
+function addCart(index) {
+    let currentuser = localStorage.getItem('currentuser') ? JSON.parse(localStorage.getItem('currentuser')) : [];
+    let soluong = document.querySelector('.input-qty').value;
+    let popupDetailNote = document.querySelector('#popup-detail-note').value;
+    let note = popupDetailNote == "" ? "Không có ghi chú" : popupDetailNote;
+    let productcart = {
+        id: index,
+        soluong: parseInt(soluong),
+        note: note
+    }
+    let vitri = currentuser.cart.findIndex(item => item.id == productcart.id);
+    if (vitri == -1) {
+        currentuser.cart.push(productcart);
+    } else {
+        currentuser.cart[vitri].soluong = parseInt(currentuser.cart[vitri].soluong) + parseInt(productcart.soluong);
+    }
+    localStorage.setItem('currentuser', JSON.stringify(currentuser));
+    updateAmount();
+    closeModal();
+    // toast({ title: 'Success', message: 'Thêm thành công sản phẩm vào giỏ hàng', type: 'success', duration: 3000 });
+}
 
